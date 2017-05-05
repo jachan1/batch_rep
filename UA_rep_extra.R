7a3eaf28fb59e25df069a40bfb4f8009776d1591
jachan1 <jachan1@gmail.com>
2017-05-05 11:56:30 -0400

    Update UA_rep_extra.R

--- UA_rep_extra.R
+++ UA_rep_extra.R
@@ -96,6 +96,8 @@ cat("\n\n______________Full Rmd Code Run___________\n\n")
 tryCatch(writeLines(readLines(bargs$rmdfile)), 
          error=function(e) sprintf("Error: Could not print code\n\n%s", e))
 print(sessionInfo())
+print("hello")
+print("hello_cat")
 sink(type="message")
 sink()
 close(con)
