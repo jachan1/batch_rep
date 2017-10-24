a5215188b2d7590c7e3f8f8a1bc37519214c9567
jachan1 <jachan1@gmail.com>
2017-10-24 13:36:09 -0400

    archive option added

--- batch_run_rep.bat
+++ batch_run_rep.bat
@@ -2,7 +2,8 @@
 :: Rscript should be given r batch script location
 :: named arguments and defaults are below. All arguments must be named and come after --args
 :: note - outloc must be relative to Rmd file
-:: portrait=T rmdfile=report.Rmd outloc=/../docs prefix=report_ wkloc=C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe
+:: archive should be set to 0 if don't want old reports to be saved in separate folder.
+:: portrait=T rmdfile=report.Rmd outloc=/../docs prefix=report_ wkloc=C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe archive=1
 
 pushd "%~dp0"
 Rscript -e "source('https://raw.githubusercontent.com/jachan1/batch_rep/master/UA_rep_extra.R')" --args rmdfile=analysis.Rmd prefix=Report_
